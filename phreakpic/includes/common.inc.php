4c0ea2d58eb13ca211058ed3839eb29a7b20b4af
jascha <jascha>
2002-09-03 18:01:42 +0000

    fix require path bug

--- phreakpic/includes/common.inc.php
+++ phreakpic/includes/common.inc.php
@@ -1,14 +1,14 @@
 <?php
 
 require_once('DB.php');
-require_once('config.inc.php');
+require_once('../config.inc.php');
 require_once('errorhandling.inc.php');
 
 //Check if connected to database, if not connect to it
 
 if (!isset($db))
 {
-	$dsn = "$config_vars['db_type']://$config_vars['db_user']:$config_vars['db_passwd']@$config_vars['db_host']/$config_vars['db_name']";
+	$dsn = $config_vars['db_type'].'://'.$config_vars['db_user'].':'.$config_vars['db_passwd'].'@'.$config_vars['db_host'].'/'.$config_vars['db_name'];
 	
 	// connect to database presisent
 	$db = DB::connect($dsn, true);
