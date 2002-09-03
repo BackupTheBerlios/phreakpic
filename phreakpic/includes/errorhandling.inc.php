95f7c9cf9adae9d53222b4e870dc0673575b3c90
jascha <jascha>
2002-09-03 18:05:36 +0000

    removed spaces after ?>

--- phreakpic/includes/errorhandling.inc.php
+++ phreakpic/includes/errorhandling.inc.php
@@ -4,4 +4,4 @@ function message_die($msg_code, $msg_location = '', $msg_describtion = '',$msg_e
 {
 	echo ("$msg_code $msg_text $msg_titel in line $err_line in file $err_file at sql querry $sql<br>");
 }
-?> 
+?>
