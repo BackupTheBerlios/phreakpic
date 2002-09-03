4590eee6abddb9931976b87edb7657c7a77ae197
ansgar <ansgar>
2002-09-03 22:27:33 +0000

    ein paar funktionen "ausgemalt"

--- phreakpic/modules/pic_managment/interface.inc.php
+++ phreakpic/modules/pic_managment/interface.inc.php
@@ -3,38 +3,121 @@ require_once('includes/common.inc.php');
 
 // Get Functions
 
-function get_cats($parent_id)
+function get_cats($parent_id, $requested_fields)
 {
-// Retruns an Array of cat_ids. Of all categories which are under the categorie with ihe id $parent_id
+	// Returns an Array of cat_ids and their names. Of all categories which are under the categorie with ihe id $parent_id
+   global $db;
+   global $config_vars;
+
+   $sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "cats WHERE parent_id = '$parent_id'";
+
+   if (!$result = $db->query($sql))
+   {
+   	message_die("", "Beim auswählen der Kategorien ", "Datenbankabfrage schlug fehl<br>", $result->getMessage(), "", __LINE__, __FILE__);
+   }
+
+   while ($row = $db->sql_fetchrow($result))
+   {
+      $cat_data[] = $row;
+   }
+
+
+   return $cat_data;
 
 }
 
-function get_pics_of_cat($cat_id)
+function get_pics_of_cat($cat_id, $requested_fields)
 {
-// Returns an Array of pic_ids of all Pictures which are in the categorie with id $cat_id
+	// Returns an Array of pic_ids of all Pictures which are in the categorie with id $cat_id
+   global $db;
+	global $config_vars;
+
+   $sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "pics WHERE cat_id = '$cat_id'";
+
+   if (!$result = $db->query($sql))
+   {
+   	message_die("", "Beim auswählen der Bilder ", "Datenbankabfrage schlug fehl<br>", $result->getMessage(), "", __LINE__, __FILE__);
+   }
+
+   while ($row = $db->sql_fetchrow($result))
+   {
+      $pic_data[] = $row;
+   }
+
+
+   return $pic_data;
+
 }
 
-function get_pics_from_sql($sql)
+function get_pics_from_sql($sql_where_clause, $requested_fields)
 {
 // Returns an Array of pic_ids of the pictures that are returned by the sql querry $sql
+   global $db;
+	global $config_vars;
+
+   $sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "pics WHERE $sql_where_clause";
+
+   if (!$result = $db->query($sql))
+   {
+   	message_die("", "Beim auswählen der Bilder mit eigener WHERE Clause ", "Datenbankabfrage schlug fehl<br>", $result->getMessage(), "", __LINE__, __FILE__);
+   }
+
+   while ($row = $db->sql_fetchrow($result))
+   {
+      $pic_data[] = $row;
+   }
+
+
+   return $pic_data;
+
 }
 
-function get_series_of_cat($cat_id)
+function get_series_of_cat($cat_id, $requested_fields)
 {
 // Return an Array of serie_ids of the series that are in the categorie $cat_id
+   global $db;
+	global $config_vars;
+
+   $sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "series WHERE cat_id = '$cat_id'";
+
+   if (!$result = $db->query($sql))
+   {
+   	message_die("", "Beim auswählen der Serien ", "Datenbankabfrage schlug fehl<br>", $result->getMessage(), "", __LINE__, __FILE__);
+   }
+
+   while ($row = $db->sql_fetchrow($result))
+   {
+      $series_data[] = $row;
+   }
+
+   return $series_data;
+
 }
 
 function get_pics_of_serie($serie_id)
 {
 // Returns an Array of pic_ids of the pictures that are in the serie with the id $serie_id
+   global $db;
+	global $config_vars;
+
+   $sql = "SELECT pic_id FROM " . $config_vars['table_prefix'] . "pic_in_serie WHERE serie_id = '$serie_id' ORDER BY place_in_serie";
+
+   if (!$result = $db->query($sql))
+   {
+   	message_die("", "Beim auswählen der Bilder ", "Datenbankabfrage schlug fehl<br>", $result->getMessage(), "", __LINE__, __FILE__);
+   }
+
+   while ($row = $db->sql_fetchrow($result))
+   {
+      $pic_data[] = $row;
+   }
+
+
+   return $pic_data;
+
 }
 
-function get_thumb_path_of_pic($pic_id)
-{
-// Returns a path to the thumb of the pic with id $pic_id
-}
-
-function get_pic_path($pic_id)
+function get_pic_data($pic_id, $requested_fields)
 {
 // Returns the path to the pic with id $pic_id
 }
@@ -59,23 +142,22 @@ function add_pic_to_cat($pic_dir,$cat_id)
 
 // Cateogries and series
 
-function new_categorie($parent_id,$name)
+function new_category($parent_id,$name)
 {
 // Creates an new categorie with name $name under the categorie with the id $parent_id
 	global $db;
 
 	$sql = '
-		INSERT INTO '.$talbe_prefix."categories (name,parent_id)
+		INSERT INTO '.$table_prefix."categories (name,parent_id)
 		VALUES ('$name',$parent_id)";
-	
+
 	$result = $db->query($sql);
 
-	if (DB::isError($result)) 
+	if (DB::isError($result))
 	{
-		echo ("ERROR");
 		message_die("","During creation of a Categorie ","Something went wrong<br> ",  $result->getMessage(), '', __LINE__, __FILE__);
 	}
-	
+
 }
 
 function new_serie($cat_id,$name,$pictures)
@@ -85,4 +167,4 @@ function new_serie($cat_id,$name,$pictures)
 }
 
 
-?> 
+?>
