466e151e390173d5d984562f5cabcd6191e83dd3
jascha <jascha>
2002-09-03 22:30:37 +0000

    message die für ansi

--- phreakpic/modules/authorisation/interface.inc.php
+++ phreakpic/modules/authorisation/interface.inc.php
@@ -2,6 +2,26 @@
 function check_cat_allowed($cat_id,$user_id)
 {
 // Returns TRUE if the user with id $user_id is allowed to see the categorie with id $cat_id
+	global $db,$config_vars;
+
+	$sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "cats WHERE parent_id = '$parent_id'";
+
+	if (!$result = $db->query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not obtain forum watch information", '', __LINE__, __FILE__, $sql);
+	}
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$cat_data[] = $row;
+	}
+
+
+	return $cat_data;
+
+
+
+
 }
 
 function check_pic_allowed($pic_id,$user_id)
