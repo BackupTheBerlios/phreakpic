fb956f97cbcfd1d674bcd583b85ccf58cac7e72e
jascha <jascha>
2002-09-03 18:03:09 +0000

    new message_die interface

--- phreakpic/includes/errorhandling.inc.php
+++ phreakpic/includes/errorhandling.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-function message_die($msg_code, $msg_text = '', $msg_title = '', $err_line = '', $err_file = '', $sql = '')
+function message_die($msg_code, $msg_location = '', $msg_describtion = '',$msg_error, $err_line = '', $err_file = '', $sql = '')
 {
 	echo ("$msg_code $msg_text $msg_titel in line $err_line in file $err_file at sql querry $sql<br>");
 }
