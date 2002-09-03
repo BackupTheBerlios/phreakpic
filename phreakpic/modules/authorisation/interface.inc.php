172f50405ca45c7153c42dd87d3b606724428a6d
jascha <jascha>
2002-09-03 22:41:28 +0000

    get_groups_of_user implemented

--- phreakpic/modules/authorisation/interface.inc.php
+++ phreakpic/modules/authorisation/interface.inc.php
@@ -3,29 +3,39 @@ function check_cat_allowed($cat_id,$user_id)
 {
 // Returns TRUE if the user with id $user_id is allowed to see the categorie with id $cat_id
 	global $db,$config_vars;
-
-	$sql = "SELECT $requested_fields FROM " . $config_vars['table_prefix'] . "cats WHERE parent_id = '$parent_id'";
-
-	if (!$result = $db->query($sql))
-	{
-		message_die(GENERAL_ERROR, "Could not obtain forum watch information", '', __LINE__, __FILE__, $sql);
-	}
-
-	while ($row = $db->sql_fetchrow($result))
-	{
-		$cat_data[] = $row;
-	}
-
-
-	return $cat_data;
-
-
-
+	
+	
+	// check in which groups the user is
+	
+	$group_ids=get_groups_of_user($user_id);
+	
 
 }
 
 function check_pic_allowed($pic_id,$user_id)
 {
 // Returns TRUE if the user with id $user_id is allowed to see the picture with id $pic_id
+	$group_ids=get_groups_of_user($user_id);
+}
 
+// !!!!maybe not interface anymore!!!!!
+function get_groups_of_user($user_id)
+{
+	// Returns array of group_ids in which the user with id $user_id is
+	$sql = "
+		SELECT group_id from " . $config_vars['table_prefix'] . "user_in_group 
+		WHERE user_id like $user_id";
+	
+	if (!$result = $db->query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not get groups of user", '', __LINE__, __FILE__, $sql);
+	}
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$group_ids[] = $row['group_id'];
+	}
+		return $group_ids;
+
+}
 ?> 
