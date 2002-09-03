3879e6224847c24e086d655bd5443e210dde2d16
jascha <jascha>
2002-09-03 21:57:34 +0000

    changed db abstraction layer and auth to phpBB

--- phreakpic/config.inc.php
+++ phreakpic/config.inc.php
@@ -3,14 +3,6 @@
 $config_vars = array 
 (
 	//Database
-	'db_type' => 'mysql',
-	'db_user' => 'root',
-	'db_passwd' => '',
-	'db_host' => 'localhost',
-	'db_name' => 'photolib',
 	'table_prefix' => '',
-	//Session
-	'session_length' => 3600,
-	'session_name' => 'photosession' 
 );
 ?>
--- phreakpic/includes/common.inc.php
+++ phreakpic/includes/common.inc.php
@@ -1,26 +1,8 @@
 <?php
-
-require_once('DB.php');
+require_once('includes/phpbb.inc.php');
 require_once('../config.inc.php');
-require_once('errorhandling.inc.php');
+require_once('includes/errorhandling.inc.php');
 
-//Check if connected to database, if not connect to it
 
-if (!isset($db))
-{
-	$dsn = $config_vars['db_type'].'://'.$config_vars['db_user'].':'.$config_vars['db_passwd'].'@'.$config_vars['db_host'].'/'.$config_vars['db_name'];
-	
-	// connect to database presisent
-	$db = DB::connect($dsn, true);
-	
-	if (DB::isError($db)) 
-	{
-	echo ("ERROR");
-		message_die("", $db->getMessage(), '', __LINE__, __FILE__);
-	}
-	
-	// set default fetchmode to assoc
-	$db->setFetchMode(DB_FETCHMODE_ASSOC);
-}
 
 ?> 
