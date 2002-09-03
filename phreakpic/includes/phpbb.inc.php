174be70910171970d5fc1aa75f253ac9e4d7a22f
jascha <jascha>
2002-09-03 21:57:52 +0000

    initial interface to phpBB

---
+++ phreakpic/includes/phpbb.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('IN_PHPBB', true);
+$phpbb_root_path = '../phpBB2/';
+include($phpbb_root_path . 'extension.inc');
+include($phpbb_root_path . 'common.'.$phpEx);
+
+$userdata = session_pagestart($user_ip, PAGE_INDEX);
+init_userprefs($userdata);
+
+
+if ( !$userdata['session_logged_in'] )
+{
+	$redirect =  "../".$PHP_SELF;
+	$header_location = ( @preg_match("/Microsoft|WebSTAR|Xitami/", getenv("SERVER_SOFTWARE")) ) ? "Refresh: 0; URL=" : "Location: ";
+	header($header_location . append_sid($phpbb_root_path."login.$phpEx?redirect=$redirect", true));
+}
+
+?>
