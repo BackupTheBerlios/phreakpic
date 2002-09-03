63098c6969cfec577eb77c5d3ef3476f03ba492c
jascha <jascha>
2002-09-03 18:03:24 +0000

    updated to config_vars

--- phreakpic/config.inc.php
+++ phreakpic/config.inc.php
@@ -1,13 +1,16 @@
 <?php 
 
-
-//Database
-$db_type = 'mysql';
-$db_user = 'root';
-$db_passwd = '';
-$db_host = 'localhost';
-$db_name = 'photolib';
-$table_prefix = '';
-
-
+$config_vars = array 
+(
+	//Database
+	'db_type' => 'mysql',
+	'db_user' => 'root',
+	'db_passwd' => '',
+	'db_host' => 'localhost',
+	'db_name' => 'photolib',
+	'table_prefix' => '',
+	//Session
+	'session_length' => 3600,
+	'session_name' => 'photosession' 
+);
 ?>
