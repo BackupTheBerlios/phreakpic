83d90dc393277948fa332348b881bcf6131b77d7
jascha <jascha>
2002-09-03 22:23:50 +0000

    not needed anymore errorhandling done by phpBB

--- phreakpic/includes/errorhandling.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function message_die($msg_code, $msg_location = '', $msg_describtion = '',$msg_error, $err_line = '', $err_file = '', $sql = '')
-{
-	echo ("$msg_code $msg_text $msg_titel in line $err_line in file $err_file at sql querry $sql<br>");
-}
-?>
