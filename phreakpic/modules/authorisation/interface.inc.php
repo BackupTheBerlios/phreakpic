7e114b04ce3708610a5a206c114462a544f2797a
jascha <jascha>
2002-09-03 17:00:54 +0000

    initial creation function bodys with a little describtion

---
+++ phreakpic/modules/authorisation/interface.inc.php
@@ -0,0 +1,11 @@
+<?php
+function check_cat_allowed($cat_id,$user_id)
+{
+// Returns TRUE if the user with id $user_id is allowed to see the categorie with id $cat_id
+}
+
+function check_pic_allowed($pic_id,$user_id)
+{
+// Returns TRUE if the user with id $user_id is allowed to see the picture with id $pic_id
+
+?> 
