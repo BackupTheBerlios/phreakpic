fb6a0b2e7d1f059c4834eade3adbba7f92c137e1
test <test>
2002-09-08 23:15:08 +0000

    include file for generell used functions

---
+++ phreakpic/includes/functions.inc.php
@@ -0,0 +1,14 @@
+<?php
+function generate_where($field,$array)
+{
+	for ($i=0;$i<sizeof($array);$i++)
+	{
+		$where=$where." ".$field." like ".$array[$i];
+		if ($i<sizeof($array)-1)
+			$where=$where." or";
+	}
+	return $where;
+}
+
+
+?>
