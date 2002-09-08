13b836a60acab767329f690f692594745b636b29
test <test>
2002-09-08 23:14:43 +0000

    implemented all functions with the current authorisation method. (Maybe they will go into the classes photo_content and categories)

--- phreakpic/modules/authorisation/interface.inc.php
+++ phreakpic/modules/authorisation/interface.inc.php
@@ -1,32 +1,132 @@
 <?php
-function check_cat_allowed($cat_id,$user_id)
+require_once ('includes/functions.inc.php');
+
+function check_cat_action_allowed($catgroup_id,$user_id,$action)
 {
-// Returns TRUE if the user with id $user_id is allowed to see the categorie with id $cat_id
+	// Returns TRUE if the user with id $user_id is allowed to do $action with the categories in in the catgroup with id $catgroup_id
 	global $db,$config_vars;
 	
+	// check in which groups the user is
+	$group_ids=get_groups_of_user($user_id);
+	$where = generate_where('usergroup_id',$user_groups);
+	$sql = 'select usergroup_id from '.$config_vars['table_prefix']."cat_auth where ($action like 1) and (catgroup_id like $catgroup_id) and ($where) limit 1";
+	
+	if (!$result = $db->sql_query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not check whether the user is allowed to this action", '', __LINE__, __FILE__, $sql);
+	}
+
+
+	if ($db->sql_affectedrows()>=1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+	
+
+}
+
+function check_content_action_allowed($contentgroup_id,$user_id,$action)
+{
+	// Returns TRUE if the user with id $user_id is allowed to do $action with content in $contentgroup_id
+	
+	global $db,$config_vars;	
 	
 	// check in which groups the user is
+	$user_groups=get_groups_of_user($user_id);
 	
-	$group_ids=get_groups_of_user($user_id);
+	// check if there is at least one entry where in one of the $user_groups is $action allowed in $contentgroup_id
+	$where = generate_where('usergroup_id',$user_groups);
+	$sql = 'select usergroup_id from '.$config_vars['table_prefix']."content_auth where ($action like 1) and (contentgroup_id like $contentgroup_id) and ($where) limit 1";
 	
+	if (!$result = $db->sql_query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not check whether the user is allowed to this action", '', __LINE__, __FILE__, $sql);
+	}
+
+
+	if ($db->sql_affectedrows()>=1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 
 }
 
-function check_pic_allowed($pic_id,$user_id)
+function get_allowed_contentgroups_where($user_id,$action)
 {
-// Returns TRUE if the user with id $user_id is allowed to see the picture with id $pic_id
-	$group_ids=get_groups_of_user($user_id);
+	// Returns an SQL where that limits a query to the content where $action if allowed by $user_id.
+	// $action must be the name of a field out of the content_auth table, which says if this action is allowed or not
+
+	global $db,$config_vars;	
+	// first of all get the groups in which the user is.
+	$user_groups = get_groups_of_user($user_id);
+	
+	// get contentgroup_ids from the contents groups where at least one usergroup out of §users_groups is allowed to do $action
+	$where = generate_where('usergroup_id',$user_groups);
+	$sql = 'select contentgroup_id from '.$config_vars['table_prefix']."content_auth where ($action like 1) and $where";
+	
+	if (!$result = $db->sql_query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not check whether the contentgroups where this user is allowed to this action", '', __LINE__, __FILE__, $sql);
+	}
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$allowed_contentgroups[]=$row['contentgroup_id'];
+		
+	}
+
+	return generate_where('contentgroup_id',$allowed_contentgroups);	
 }
+
+function get_allowed_catgroups_where($user_id,$action)
+{
+	// Returns an SQL where that limits a query to the categories where $action if allowed by $user_id.
+	// $action must be the name of a field out of the cat_auth table, which says if this action is allowed or not
+
+	global $db,$config_vars;	
+	// first of all get the groups in which the user is.
+	$user_groups = get_groups_of_user($user_id);
+	
+	// get contentgroup_ids from the contents groups where at least one usergroup out of §users_groups is allowed to do $action
+	$where = generate_where('usergroup_id',$user_groups);
+	$sql = 'select catgroup_id from '.$config_vars['table_prefix']."cat_auth where ($action like 1) and $where";
+	
+	if (!$result = $db->sql_query($sql))
+	{
+		message_die(GENERAL_ERROR, "Could not check whether the contentgroups where this user is allowed to this action", '', __LINE__, __FILE__, $sql);
+	}
+
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$allowed_catgroups[]=$row['contentgroup_id'];
+		
+	}
+
+	return generate_where('catgroup_id',$allowed_catgroups);	
+}
+
+
+
 
 // !!!!maybe not interface anymore!!!!!
 function get_groups_of_user($user_id)
 {
+	global $db,$config_vars;
 	// Returns array of group_ids in which the user with id $user_id is
 	$sql = "
 		SELECT group_id from " . $config_vars['table_prefix'] . "user_in_group 
 		WHERE user_id like $user_id";
 	
-	if (!$result = $db->query($sql))
+	if (!$result = $db->sql_query($sql))
 	{
 		message_die(GENERAL_ERROR, "Could not get groups of user", '', __LINE__, __FILE__, $sql);
 	}
@@ -38,4 +138,4 @@ function get_groups_of_user($user_id)
 		return $group_ids;
 
 }
-?> 
+?>
