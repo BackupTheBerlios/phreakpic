47a1036dc642bbb505eb05f8a53cd2e5f2b1678a
jascha <jascha>
2002-09-09 19:52:34 +0000

    added cat deleted mode constants

--- phreakpic/includes/common.inc.php
+++ phreakpic/includes/common.inc.php
@@ -10,6 +10,11 @@ define("OP_NOT_PERMITTED", 0);
 define("OP_SUCCESSFUL", 1);
 define("OP_FAILED", 2);
 define("OP_PARENT_ID_INVALID",3);
+define("OP_NOT_IN_DB",4);
+
+// Categorie delete modes
+define("CDM_REMOVE_CONTENT",0);
+define("CDM_MOVE_CONTENT",1);
 
 
 
