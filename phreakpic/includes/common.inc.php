0717f8400ec5a556061a21d0104f6bb13295a561
jascha <jascha>
2002-09-09 19:18:57 +0000

    added defines for error handling

--- phreakpic/includes/common.inc.php
+++ phreakpic/includes/common.inc.php
@@ -3,7 +3,14 @@ require_once('includes/phpbb.inc.php');
 require_once('config.inc.php');
 
 define("GENERATE_NAMES", 1);
-define("BLANK_NAMES", 0)
+define("BLANK_NAMES", 0);
+
+// Error Messages from Objekts
+define("OP_NOT_PERMITTED", 0);
+define("OP_SUCCESSFUL", 1);
+define("OP_FAILED", 2);
+define("OP_PARENT_ID_INVALID",3);
+
 
 
 
