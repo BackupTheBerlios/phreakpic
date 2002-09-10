3c61673c31a45837fc626c41bfaa34f5e9545f66
jascha <jascha>
2002-09-10 21:11:40 +0000

    added deleted_content_cat

--- phreakpic/config.inc.php
+++ phreakpic/config.inc.php
@@ -1,16 +1,20 @@
 <?php
 $config_vars = array
 (
-   //Database
-   'table_prefix' => 'photo_',
+	//Database
+	'table_prefix' => 'photo_',
 
-   //Picture stuff
-   'thumb_size' =>
-      array
-      (
-         'percent' => '30',
-         'height' => '130',
-         'width' => '100'
-      )
+	//Picture stuff
+	'thumb_size' =>
+	array
+	(
+		'percent' => '30',
+		'height' => '130',
+		'width' => '100'
+	),
+	
+	// ID of the cat where to put pictures that are no longer linked in any cat
+	'deleted_content_cat' => 1
+    
 );
 ?>
