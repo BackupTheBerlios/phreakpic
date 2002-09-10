7cbbd2e99221167cbcc568f6d3ce29e58c4e39de
jascha <jascha>
2002-09-10 18:19:18 +0000

    more detailed error messages added

--- phreakpic/includes/common.inc.php
+++ phreakpic/includes/common.inc.php
@@ -6,11 +6,16 @@ define("GENERATE_NAMES", 1); //for functions add_dir
 define("BLANK_NAMES", 0);
 
 // Error Messages from Objekts
-define("OP_NOT_PERMITTED", 0); //return value for class functions who checks if the user is allowed to do something. If he is not, the functions returns this constant.
 define("OP_SUCCESSFUL", 1);
 define("OP_FAILED", 2);
 define("OP_PARENT_ID_INVALID",3);
 define("OP_NOT_IN_DB",4);
+// Not Permitted (NP) constants
+define("OP_NP_MISSING_CAT_MOVE", 0); //return value for class functions who checks if the user is allowed to do something. If he is not, the functions returns this constant.
+define("OP_NP_MISSING_CAT_ADD", 5);
+define("OP_NP_MISSING_EDIT", 6);
+define("OP_NP_MISSING_DELETE", 7);
+
 
 //Class vars
 define("NOT_SUPPORTED", 1); //return value for dummy functions in classes. When function will be not implemented in the extended object, this will be returned.
