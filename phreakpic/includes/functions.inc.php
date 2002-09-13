28218a31dd7595b0a5538ee455b78e0b6e725a60
jascha <jascha>
2002-09-13 23:20:41 +0000

    added new functions

--- phreakpic/includes/functions.inc.php
+++ phreakpic/includes/functions.inc.php
@@ -15,6 +15,50 @@ function getext($in_file)
 	return(end(explode('.',$in_file)));
 }
 
+function getfile($in_file)
+{
+	return substr($in_file,0,strrpos($in_file,"."));	
+}
 
+/*function linkencode ($p_url) {
+	// needs php 4.1.0
+ $ta = parse_url($p_url);
+ if (!empty($ta[scheme])) { $ta[scheme].='://'; }
+ if (!empty($ta[pass]) and !empty($ta[user])) {
+ $ta[user].=':';
+ $ta[pass]=rawurlencode($ta[pass]).'@';
+ } elseif (!empty($ta[user])) {
+ $ta[user].='@';
+ }
+ if (!empty($ta[port]) and !empty($ta[host])) {
+ $ta[host]=''.$ta[host].':';
+ } elseif (!empty($ta[host])) {
+ $ta[host]=$ta[host];
+ }
+ if (!empty($ta[path])) {
+ $tu='';
+ $tok=strtok($ta[path], "\\/");
+ while (strlen($tok)) {
+ $tu.=rawurlencode($tok).'/';
+ $tok=strtok("\\/");
+ }
+ $ta[path]='/'.trim($tu, '/');
+ }
+ if (!empty($ta[query])) { $ta[query]='?'.$ta[query]; }
+ if (!empty($ta[fragment])) { $ta[fragment]='#'.$ta[fragment]; }
+ return implode('', array($ta[scheme], $ta[user], $ta[pass], $ta[host], $ta[port], $ta[path], $ta[query], $ta[fragment]));
+}*/
+
+function  linkencode($str)
+{
+	// for php < 4.1.0
+	$str=ereg_replace(" ","%20",$str);
+	$str=ereg_replace("&","%26",$str);
+	$str=ereg_replace("\+","%2b",$str);
+	$str=ereg_replace("ü","%fc",$str);
+	$str=ereg_replace("Ü","%dc",$str);
+
+	return $str;
+}
 
 ?>
