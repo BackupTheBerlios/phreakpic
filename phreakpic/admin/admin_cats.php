6449684f2995aed42cc033ea1a664a60319421f0
ansgar <ansgar>
2002-09-13 21:47:47 +0000

    inital

---
+++ phreakpic/admin/admin_cats.php
@@ -0,0 +1,38 @@
+<?php
+require_once('includes/common.inc.php');
+require_once('./classes/album_content.inc.php');
+require_once('./includes/template.inc.php');
+/*	admin_cats:
+* 	$mode can have following content:
+* 		view
+* 		add
+* 		edit
+* 		delete
+* 		move
+*/
+
+if($mode == "view")
+{
+	
+}	//end of $mode = view
+elseif($mode == "add")
+{
+	
+}	//end of $mode = add
+elseif($mode == "edit")
+{
+	
+}	//end of $mode = edit
+elseif($mode == "delete")
+{
+	
+}	//end of $mode = delete
+elseif($mode == "move")
+{
+	
+}	//end of $mode = mode
+else	//if $mode is nothing, do nothing.
+{
+	
+}
+?>
---
+++ phreakpic/admin/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('includes/common.inc.php');
+require_once('./classes/album_content.inc.php');
+require_once('./includes/template.inc.php');
+/*	HIer kommt die Übersicht der Funktionen rein und vielleicht etwas Statistiken
+* 
+* 
+* Funktionesweise:
+* $mode= left ist der linke Framerahmen
+* 
+*/
+
+
+?>
