63a4412c0534b4406d1a34389d5867654f7321de
ansgar <ansgar>
2002-09-14 03:21:26 +0000

    made it useable!!!

--- phreakpic/view_content.php
+++ phreakpic/view_content.php
@@ -2,17 +2,33 @@
 require_once('./includes/common.inc.php');
 require_once('./classes/album_content.inc.php');
 require_once('./includes/template.inc.php');
-$picture = new picture();
-$result = $picture->generate_from_id('4');
-if ($result != OP_SUCCESSFUL)
+include_once('./modules/pic_managment/interface.inc.php');
+
+$content = get_content_object_from_id($content_id);
+if (!is_object($content))
 {
 	message_die(GENERAL_ERROR, "Could not generate content from id", '', __LINE__, __FILE__, $sql);
 }
 
+$surrounding_content = $content->get_surrounding_content($cat_id);
 
-$smarty->assign('file',$picture->get_file());
-$smarty->assign('content_size',$content->get_content_size()); //thats the height and width of the object...
-$smaery->assing('id',$id);
-$smarty->display($userdata['photo_user_template'].'/test_view_pic.php.tpl');
 
+if (is_object($surrounding_content['prev']))
+{
+	$smarty->assign('is_prev_content', true);
+	$smarty->assign('prev_thumb',$surrounding_content['prev']->get_thumb());
+}
+
+if (is_object($surrounding_content['next']))
+{
+	$smarty->assign('is_next_content', true);
+	$smarty->assign('next_thumb',$surrounding_content['next']->get_thumb());
+}
+
+$smarty->assign('html', $content->get_html());
+$smarty->assign('name', $content->get_name());
+$smarty->assign('cat_id', $cat_id);
+//$smarty->assign('content_size',$content->get_content_size()); //thats the height and width of the object...
+//$smarty->assign('id',$id);
+$smarty->display($userdata['photo_user_template'].'/view_content.tpl');
 ?>
