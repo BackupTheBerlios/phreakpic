326f9d63330f375be7fc866e9d90575598dd2145
ansgar <ansgar>
2002-09-14 03:19:53 +0000

    added debugging vars

--- phreakpic/includes/template.inc.php
+++ phreakpic/includes/template.inc.php
@@ -1,12 +1,1 @@
-<?php
-	require_once('./includes/common.inc.php');
-	require_once(SMARTY_DIR.'Smarty.class.php');
-
-	$smarty = new Smarty;
-	$smarty->template_dir = './templates/';
-	$smarty->compile_dir = './templates_c/';
-	$smarty->config_dir = './templates/';
-	$smarty->cache_dir = './smarty_cache/';
-	$smarty->left_delimiter = '<!--{';
-	$smarty->right_delimiter = '}-->';
-?>
+<?php	include_once('./includes/common.inc.php');	include_once(SMARTY_DIR.'Smarty.class.php');	$smarty = new Smarty;	$smarty->template_dir = './templates/';	$smarty->compile_dir = './templates_c/';	$smarty->config_dir = './templates/';	$smarty->cache_dir = './smarty_cache/';	$smarty->left_delimiter = '<!--{';	$smarty->right_delimiter = '}-->';	$smarty->force_compile = '1';	$smarty->debugging = true;?>
