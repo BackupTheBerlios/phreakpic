85d1daabb913a1130d47e1f84e606a1329104c12
jascha <jascha>
2002-09-14 03:08:37 +0000

    new thumb_size var

--- phreakpic/config.inc.php
+++ phreakpic/config.inc.php
@@ -10,17 +10,21 @@ $config_vars = array
 	'thumb_size' =>
 	array
 	(
-		'percent' => '30',
-		'height' => '130',
-		'width' => '100'
+	//	'percent' => '30',
+	//	'height' => '130',
+	//	'width' => '100'
+		'maxsize' => '130'
 	),
+	
 	
 	// ID of the cat where to put pictures that are no longer linked in any cat
 	'deleted_content_cat' => 1,
 	
 	'root_categorie' => 0,
 	
-	'dir_mask' => 0777
-    
+	'dir_mask' => 0775,
+   
+	//view_cat.php the Colums of the table, where we can see the thumbnails
+	'thumb_table_cols' => 4
 );
 ?>
