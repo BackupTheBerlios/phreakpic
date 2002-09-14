494eac929214867a7fcaca8c9e37c4599d3f83a2
ansgar <ansgar>
2002-09-14 23:20:21 +0000

    initial release

---
+++ phreakpic/languages/german/lang_main.php
@@ -0,0 +1,11 @@
+<?php
+/*Here comes all Languagestring from the files
+* view_content.php	view_cat.php
+* 
+*/
+
+
+//view_content.tpl
+$lang['home'] = 'Start';
+$lang['add_comment'] = 'Kommentar hinzufügen';
+?>
