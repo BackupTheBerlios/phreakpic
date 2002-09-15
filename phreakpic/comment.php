84e62b08dfe9e66368029a6a55e6711161c26325
jascha <jascha>
2002-09-15 13:39:41 +0000

    created the add/edit comment page

---
+++ phreakpic/comment.php
@@ -0,0 +1,17 @@
+<?php
+	include_once ("includes/common.inc.php");
+	include_once ('classes/user_feedback.inc.php');
+	include_once('./includes/template.inc.php');
+	
+	$smarty->assign('mode', $mode);
+	$smarty->assign('type', $type);
+	$smarty->assign('parent_id', $parent_id);
+	$smarty->assign('cat_id', $cat_id);
+	
+	if (isset($content_id))
+	{
+		$smarty->assign('oontent_id', $content_id);
+		$smarty->assign('oontent_id_string', "&content_id=$content_id");
+	}
+	$smarty->display($userdata['photo_user_template'].'/comment.tpl');
+?>
