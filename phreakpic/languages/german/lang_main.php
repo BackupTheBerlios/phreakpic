2adb4e9b9c37a1620093c92983c0aefce7d7c40c
Tesla2k <Tesla2k>
2002-09-23 11:52:10 +0000

    more vars

--- phreakpic/languages/german/lang_main.php
+++ phreakpic/languages/german/lang_main.php
@@ -11,9 +11,9 @@ $lang['home'] = 'Start';
 //comments
 $lang['add_comment'] = 'Kommentar hinzufügen';
 $lang['wrote_at'] = 'schrieb um';
-$lang[''] = '';
-$lang[''] = '';
-$lang[''] = '';
+$lang['topic'] = 'Topic';
+$lang['text'] = 'Text';
+$lang['send'] = 'Abschicken';
 $lang[''] = '';
 $lang[''] = '';
 $lang[''] = '';
@@ -29,8 +29,8 @@ $lang['name'] = 'Name';
 $lang['place_in_cat'] = 'Platz in Kategorie';
 $lang['lock'] = 'Sperren';
 $lang['delete'] = 'Löschen';
-$lang['unlink'] = 'unlink';
-$lang['link'] = 'link';
+$lang['unlink'] = 'Von Categorie entfernen';
+$lang['link'] = 'Zu andere Categorie hinzufügen';
 $lang['move'] = 'verschieben';
 $lang['to_cat'] = 'Zu Kategorie';
 $lang['rating'] = 'Bewertung';
@@ -38,12 +38,46 @@ $lang['views'] = 'Views';
 $lang['edit_content'] = 'Editier dieses Bild';
 
 //view_cat.tpl
+$lang['description'] = 'Beschreibung';
+$lang['amount'] = 'Anzahl';
+$lang['no_subcategories'] = 'Keine Unterkategorien vorhanden';
+$lang['no_content'] = 'Keine Bilder vorhanden';
+$lang['add_new_cat'] = 'Neue Kategorie hinzufügen';
+$lang['add_content'] = 'Bild hinzufügen';
+$lang['is_serie'] = 'Ist eine Serie';
+$lang['create'] = 'Erstellen';
+$lang['edit'] = 'Editieren';
+$lang['commit'] = 'Abschicken';
+$lang['file'] = 'Datei';
+
 
 
 //admin/auths.php
 $lang['usergroups'] = 'Benutzergruppen';
 $lang['perms'] = 'Rechte';
 $lang['contentgroups'] = 'Content Gruppen';
+$lang['contentgroup'] = 'Content Gruppe';
 $lang['catgroups'] = 'Kategorie Gruppen';
+$lang['catgroup'] = 'Kategorie Gruppe';
+$lang['view'] = 'Anschauen';
+$lang['comment_edit'] = 'Kommentare Editieren';
+$lang['delete_auth'] = 'Auth Löschen';
+$lang['create_auth'] = 'Auth Erstellen';
+$lang['new_contentgroup'] = 'Contentgruppe erstellen';
+$lang['new_catgroup'] = 'Kategoriegruppe erstellen';
+$lang['cat_add'] = 'Kategorien hinzufügen';
+$lang['content_add'] = 'Content hinzufügen';
+$lang['content_remove'] = 'Content entfernen';
+$lang['cat_remove'] = 'Categorien hinzufügen';
+
+
+
+//admin/user_in_group
+$lang['new_usergroup'] = 'Neue Benutzergruppe erstellen';
+$lang['add_user_to_group'] = 'Benutzer in Gruppen hinzufügen';
+$lang['users_not_in_group'] = 'Benutzer, die nicht in der Gruppe sind';
+$lang['users_in_group'] = 'Benutzer, die in der Gruppe sind';
+
+
 
 ?>
