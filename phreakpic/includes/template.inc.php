71ec4515e14d989d97b0fb9f42df9a6808052bea
Tesla2k <Tesla2k>
2002-09-23 11:51:50 +0000

    lang includede already here

--- phreakpic/includes/template.inc.php
+++ phreakpic/includes/template.inc.php
@@ -1,6 +1,7 @@
 <?php
 	include_once(ROOT_PATH . 'includes/common.inc.php');
 	include_once(SMARTY_DIR.'Smarty.class.php');
+	include_once(ROOT_PATH . './languages/'.$userdata['user_lang'].'/lang_main.php');
 
 	$smarty = new Smarty;
 	$smarty->template_dir = ROOT_PATH . 'templates/';
