98fa92a3c46aeb5b1fce340e444ddd8e9957969f
Tesla2k <Tesla2k>
2002-10-08 19:48:19 +0000

    added default usergroups and admin allow everything

--- phreakpic/modules/authorisation/interface.inc.php
+++ phreakpic/modules/authorisation/interface.inc.php
@@ -24,8 +24,17 @@ function check_cat_action_allowed($catgroup_id,$user_id,$action)
 	// Returns TRUE if the user with id $user_id is allowed to do $action with the categories in in the catgroup with id $catgroup_id
 	global $db,$config_vars;
 	
+	// if the current user is admin allow everything
+	if ($userdata['user_level'] == ADMIN)
+	{
+		return true;
+	}
+	
 	// check in which groups the user is
 	$usergroup_ids=get_groups_of_user($user_id);
+	
+	// add the default usergroups
+	$usergroup_ids=array_merge($usergroup_ids,$config_vars['default_usergroup_ids']);
 
 	// if the user is in no usergroup then disallow the action
 	if (!isset($usergroup_ids))
@@ -61,11 +70,22 @@ function check_content_action_allowed($contentgroup_id,$user_id,$action)
 	
 	global $db,$config_vars;	
 	
+	// if the current user is admin allow everything
+	if ($userdata['user_level'] == ADMIN)
+	{
+		return true;
+	}
+
+		
+	
 	// check in which groups the user is
-	$user_groups=get_groups_of_user($user_id);	
+	$usergroups_ids=get_groups_of_user($user_id);	
 	// check if there is at least one entry where in one of the $user_groups is $action allowed in $contentgroup_id
 	
-	$where = generate_where('usergroup_id',$user_groups);
+	// add the default usergroups
+	$usergroup_ids=array_merge($usergroup_ids,$config_vars['default_usergroup_ids']);
+	
+	$where = generate_where('usergroup_id',$usergroup_ids);
 
 	$sql = 'select usergroup_id from '.$config_vars['table_prefix']."content_auth where (`$action` like 1) and (contentgroup_id like $contentgroup_id) and ($where) limit 1";
 	
@@ -92,6 +112,14 @@ function get_allowed_contentgroups_where($user_id,$action,$field='contentgroup_i
 	// $action must be the name of a field out of the content_auth table, which says if this action is allowed or not
 
 	global $db,$config_vars;	
+	
+	
+	// if the current user is admin allow everything
+	if ($userdata['user_level'] == ADMIN)
+	{
+		return '1';
+	}
+
 	// first of all get the groups in which the user is.
 //	$user_groups = get_groups_of_user($user_id);
 	
@@ -108,8 +136,11 @@ function get_allowed_contentgroups_where($user_id,$action,$field='contentgroup_i
 //	$sql = 'select contentgroup_id from '.$config_vars['table_prefix']."content_auth where ($action like 1) and $where";
 
 // EXPERIMENTAL SQL which doesnt needs get_groups_of_user
+	$default_usergroups_where = generate_where('uig.group_id',$config_vars['default_usergroup_ids']);
+	
+                                           
 	$sql ='SELECT auth.contentgroup_id, uig.group_id FROM '.$config_vars['table_prefix'].'content_auth as auth, '.$config_vars['table_prefix']."user_in_group AS uig 
-	WHERE ($action like 1) and (uig.user_id = $user_id) and (auth.usergroup_id=uig.group_id) group by auth.contentgroup_id";
+	WHERE ($action like 1) and ((uig.user_id = $user_id) or ($default_usergroups_where)) and (auth.usergroup_id=uig.group_id) group by auth.contentgroup_id";
 
 	
 	if (!$result = $db->sql_query($sql))
@@ -135,12 +166,27 @@ function get_allowed_contentgroups_where($user_id,$action,$field='contentgroup_i
 function get_allowed_catgroups_where($user_id,$action,$field='catgroup_id')
 {
 	// Returns an SQL where that limits a query to the categories where $action if allowed by $user_id.
-	// $action must be the name of a field out of the cat_auth table, which says if this action is allowed or not
-
+	// $action must be the name of a field out of the cat_auth table, which says if this action is allowed or no
+	
 	global $db,$config_vars;	
+	
+	// if the current user is admin allow everything
+	if ($userdata['user_level'] == ADMIN)
+	{
+		return '1';
+	}
+	
 	// first of all get the groups in which the user is.
-	$user_groups = get_groups_of_user($user_id);
-	if (!isset($user_groups))
+	$usergroup_ids = get_groups_of_user($user_id);
+	
+		// add the default usergroups
+	$usergroup_ids=array_merge($usergroup_ids,$config_vars['default_usergroup_ids']);
+
+	
+	
+	
+	
+	if (!isset($usergroup_ids))
 	{
 		// user is in now usergroups return 
 		return '0';
@@ -148,7 +194,7 @@ function get_allowed_catgroups_where($user_id,$action,$field='catgroup_id')
 	
 	
 	// get contentgroup_ids from the contents groups where at least one usergroup out of §users_groups is allowed to do $action
-	$where = generate_where('usergroup_id',$user_groups);
+	$where = generate_where('usergroup_id',$usergroup_ids);
 	$sql = 'select catgroup_id from '.$config_vars['table_prefix']."cat_auth where ($action like 1) and $where";
 	
 	if (!$result = $db->sql_query($sql))
@@ -188,32 +234,34 @@ function get_groups_of_user($user_id)
 		$group_ids[] = $row['group_id'];
 		
 	}
+	
 	return $group_ids;
 
 }
 
-function get_user_group_objects()
-{
-	global $db,$config_vars;
-	// Returns array of group_ids in which the user with id $user_id is
-	$sql = "SELECT id from " . $config_vars['table_prefix'] . "usergroups";
-	
-	if (!$result = $db->sql_query($sql))
-	{
-		message_die(GENERAL_ERROR, "Could not get groups of user", '', __LINE__, __FILE__, $sql);
-	}
-
-	while ($row = $db->sql_fetchrow($result))
-	{
-		$obj = new usergroup;
-		$obj->genereate_from_id($row['group_id']);
-		$groups[]=$obj;	
-		
-	}
-	return $groups;
-
-
-		
-}
+// function get_user_group_objects()
+// {
+// 	// Returns array of group_ids in which the user with id $user_id is
+// 	global $db,$config_vars;
+// 	
+// 	$sql = "SELECT id from " . $config_vars['table_prefix'] . "usergroups";
+// 	
+// 	if (!$result = $db->sql_query($sql))
+// 	{
+// 		message_die(GENERAL_ERROR, "Could not get groups of user", '', __LINE__, __FILE__, $sql);
+// 	}
+// 
+// 	while ($row = $db->sql_fetchrow($result))
+// 	{
+// 		$obj = new usergroup;
+// 		$obj->genereate_from_id($row['group_id']);
+// 		$groups[]=$obj;	
+// 		
+// 	}
+// 	return $groups;
+// 
+// 
+// 		
+// }
 
 ?>
