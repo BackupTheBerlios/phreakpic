35056065fbdcdf3963040a7ea0f3e364554ce30e
Tesla2k <Tesla2k>
2002-10-12 13:22:15 +0000

    based on 1.11 of german/lang_main.php

---
+++ phreakpic/languages/english/lang_main.php
@@ -0,0 +1,100 @@
+<?php
+/*Here comes all Languagestring from the files
+* view_content.php	view_cat.php
+* 
+*/
+
+//Header
+$lang['login'] = 'Login';
+$lang['logout'] = 'logout';
+$lang['forum'] = 'Forum';
+$lang['website_home'] = 'Home';
+//in more than one file
+$lang['home'] = 'Start';
+
+//footer
+$lang['goto_admin'] = 'Administration';
+
+
+//comments
+$lang['add_comment'] = 'Add Comment';
+$lang['wrote_at'] = 'wrote at';
+$lang['topic'] = 'topic';
+$lang['text'] = 'text';
+$lang['send'] = 'Send';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+
+
+//view_content.tpl
+$lang['nav_back'] = 'back';
+$lang['nav_next'] = 'next';
+$lang['name'] = 'name';
+$lang['place_in_cat'] = 'Place in categorie';
+$lang['lock'] = 'Lock';
+$lang['delete'] = 'Delete';
+$lang['unlink'] = 'Remove from categorie';
+$lang['link'] = 'Add to categorie';
+$lang['move'] = 'move';
+$lang['to_cat'] = 'to categorie';
+$lang['rating'] = 'rating';
+$lang['views'] = 'Views';
+$lang['edit_content'] = 'Edit this content';
+$lang['rotate'] = 'Rotate';
+$lang['rotate_free'] = 'Free / Not';
+$lang['rotate_left'] = 'left';
+$lang['rotate_right'] = 'right';
+$lang['rotate_180'] = '180';
+
+
+//view_cat.tpl
+$lang['description'] = 'Description';
+$lang['amount'] = 'amount';
+$lang['no_subcategories'] = 'No subcategories';
+$lang['no_content'] = 'No content';
+$lang['add_new_cat'] = 'Add new categorie';
+$lang['add_content'] = 'Add content';
+$lang['is_serie'] = 'Is a serie';
+$lang['create'] = 'Creata';
+$lang['edit'] = 'Edit';
+$lang['commit'] = 'Commit';
+$lang['file'] = 'File';
+
+
+
+//admin/auths.php
+$lang['usergroups'] = 'Usergroups';
+$lang['perms'] = 'Permissions';
+$lang['contentgroups'] = 'Contentgroups';
+$lang['contentgroup'] = 'Contentgroup';
+$lang['catgroups'] = 'Categoriegroups';
+$lang['catgroup'] = 'Categoriegroup';
+$lang['view'] = 'view';
+$lang['comment_edit'] = 'edit comment';
+$lang['delete_auth'] = 'delete Auth';
+$lang['create_auth'] = 'create Auth';
+$lang['new_contentgroup'] = 'create contentgroup';
+$lang['new_catgroup'] = 'create categoriegorup';
+$lang['cat_add'] = 'add categorie';
+$lang['content_add'] = 'add content';
+$lang['content_remove'] = 'delete content';
+$lang['cat_remove'] = 'add categorie';
+$lang['add_to_group'] = 'add to group';
+$lang['remove_from_group'] = 'remove from group';
+$lang['change_group'] = 'change group';
+
+
+
+//admin/user_in_group
+$lang['new_usergroup'] = 'Create new user';
+$lang['add_user_to_group'] = 'Add user to group';
+$lang['users_not_in_group'] = 'User not in group';
+$lang['users_in_group'] = 'User in group';
+
+
+
+?>
