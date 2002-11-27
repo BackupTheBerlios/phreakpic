8177d46f06d86ed2f40f4d93ef789e016f2b62a2
POilf <POilf>
2002-11-27 18:18:07 +0000

    jascha: possible to limit content per page and created a navigation for it

--- phreakpic/view_cat.php
+++ phreakpic/view_cat.php
@@ -76,7 +76,7 @@ if (isset($child_cats))
 		$child_cat_infos[$i]['name'] = $child_cats[$i]->get_name();
 		$child_cat_infos[$i]['description'] = $child_cats[$i]->get_description();
 		$child_cat_infos[$i]['content_amount'] = $child_cats[$i]->get_content_amount();
-		$child_cat_infos[$i]['content_child_amount'] = $child_cats[$i]->get_child_content_amount() - $child_cat_infos[$i]['content_amount'];
+		$child_cat_infos[$i]['content_child_amount'] = $child_cats[$i]->get_child_content_amount();
 		$child_cat_infos[$i]['current_rating'] = $child_cats[$i]->get_current_rating();
 		
 	}
@@ -162,202 +162,48 @@ if (check_cat_action_allowed($category->get_catgroup_id(),$userdata['user_id'],'
 
 
 
+if (!isset($first_content))
+{
+	$first_content = 0;
+}
 
+$contents = get_content_of_cat($cat_id,$first_content,$userdata['content_per_page']);
 
-$contents = get_content_of_cat($cat_id);
 include "includes/view_thumbs.php";
 
-/*
-if (is_array($contents))
+
+// build navigtion 
+$i=0;
+while (($i*$userdata['content_per_page'])<$category->get_content_amount())
 {
-	//editing the contents
-	if ((isset($submit)) and ($HTTP_POST_VARS['mode'] == 'edited'))
-	{
-		$add_to_cats = get_cats_data_where_perm('id,name','content_add');
-		// loop through all content
-		for ($i = 0; $i < sizeof($contents); $i++)	
-		{
-			
-			
-			// set everything for edit
-			if ($contents[$i]->check_perm('edit'))
-			{	
-				// name
-				if ($contents[$i]->set_name($HTTP_POST_VARS['name'][$i]) != OP_SUCCESSFUL)
-				{
-					die('Konnte Name '.$HTTP_POST_VARS['name'][$i].' von '.$HTTP_POST_VARS['content_id'][$i].' nicht setzen ('.$i);
-				}
-				// place_in_cat
-				
-				if ($contents[$i]->set_place_in_cat($cat_id,$HTTP_POST_VARS['place_in_cat'][$i]) != OP_SUCCESSFUL)
-				{
-					die('Konnte Place in cat '.$HTTP_POST_VARS['place_in_cat'][$i].' von '.$HTTP_POST_VARS['content_id'][$i].' nicht setzen ('.$i);
-				}
-				
-				// lock
-				
-				if ($HTTP_POST_VARS['lock'][$i] == 'on')
-				{
-					
-					if ($contents[$i]->lock() != OP_SUCCESSFUL)
-					{
-						die('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht locken');
-					}	
-				}
-				else
-				{
-					$contents[$i]->unlock();
-				}
-				
-				//rotate
-				
-				if ($HTTP_POST_VARS['rotate_mode'][$i] == 'free')
-				{
-					if (intval($HTTP_POST_VARS['rotate'][$i])!=0) 
-					{
-						$contents[$i]->rotate($HTTP_POST_VARS['rotate'][$i]);
-					}
-				}
-				else
-				{
-					$contents[$i]->rotate($HTTP_POST_VARS['rotate_mode'][$i]);
-				}
-		
-			}
-			
-			
-			// check unlink
-			if (check_cat_action_allowed($category->get_catgroup_id(),$userdata['user_id'],'content_remove'))
-			{
-				if ($HTTP_POST_VARS['unlink'][$i] == 'on')
-				{
-				
-					if ($contents[$i]->remove_from_cat($cat_id) != OP_SUCCESSFUL)
-					{
-						die ('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht von der cat entfernen');
-					}
-				}				
-			}
-			
-			// check link
-			
-			if (is_array($add_to_cats))
-			{
-				// echo "I: ".$i." id ".$contents[$i]->id." link: ".$HTTP_POST_VARS['link'][$i]."<br>";
-				if ($HTTP_POST_VARS['link'][$i] == 'on')
-				{
-					if ($contents[$i]->add_to_cat($HTTP_POST_VARS['to_cat']) != OP_SUCCESSFUL)
-					{
-						die ('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht linken');
-					}
-				}
-				// check if you have content remove rights
-				if (check_cat_action_allowed($category->get_catgroup_id(),$userdata['user_id'],'content_remove'))
-				{
-					if ($HTTP_POST_VARS['move'][$i] == 'on')
-					{
-						if ($contents[$i]->add_to_cat($HTTP_POST_VARS['to_cat']) != OP_SUCCESSFUL)
-						{
-							die ('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht moven (add)');
-						}
-						if ($contents[$i]->remove_from_cat($cat_id) != OP_SUCCESSFUL)
-						{
-							die ('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht moven (remove)');
-						}
-					}
-				}
-					
-			}
-			
-			// check change group
-			if ($HTTP_POST_VARS['change_group'][$i] == 'on')
-			{
-				
-				$contents[$i]->set_contentgroup_id($HTTP_POST_VARS['to_contengroup']);
-				
-			}
-			
-			$contents[$i]->commit();
-			// check delete
-			if ($contents[$HTTP_POST_VARS['place_in_array'][$i]]->check_perm('delete'))
-			{
-				if ($HTTP_POST_VARS['delete'][$i] == 'on')
-				{
-				
-					if ($contents[$i]->delete() != OP_SUCCESSFUL)
-					{
-						die('Konnte '.$HTTP_POST_VARS['name'][$i].' nicht löschen');
-					}	
-				}
-			}
-			
-			
-		
-			
-			
-		}
-		$smarty->assign('mode','view');
-		$smarty->assign('edited',true);
-		$contents = get_content_of_cat($cat_id);
-	}
-	
-	
+	$cat_nav_links[] = $i * $userdata['content_per_page'];
+	$i++;
+}
 
-	
-	//show thumbnails and get some infos about the content
-	for ($i = 1; $i <= sizeof($contents); $i++)
-	{
-		$thumb_infos = $contents[$i-1]->get_thumb();
-		if ($mode == 'edit')
-		{
-		
+$smarty->assign('cat_nav_links',$cat_nav_links);
+$smarty->assign('first_content',$first_content);
 
-			
-			// check if user is allowed to unlink from thie cat
-			$smarty->assign('allow_content_remove',check_cat_action_allowed($category->get_catgroup_id(),$userdata['user_id'],'content_remove'));
+if ($first_content+$userdata['content_per_page']>$category->get_content_amount())
+{
+	$smarty->assign('first_content_next',0);
+}
+else
+{
+	$smarty->assign('first_content_next',$first_content+$userdata['content_per_page']);
+}
 
-			
+if ($first_content-$userdata['content_per_page']<0)
+{
+	$smarty->assign('first_content_prev',(int)($category->get_content_amount()/$userdata['content_per_page'])*$userdata['content_per_page']);
+}
+else
+{
+	$smarty->assign('first_content_prev',$first_content-$userdata['content_per_page']);
+}
 
-			// Check if user has rights to add content to a cat
-			$add_to_cats = get_cats_data_where_perm('id,name','content_add');
-			if (is_array($add_to_cats))
-			{
-					$smarty->assign('allow_link',true);	
-					$smarty->assign('add_to_cats',$add_to_cats);
-			}
-			
-			// Check if user has the right to remove the content from the contentgroup
-			$thumb_infos['allow_remove_from_group'] = $contents[$i-1]->check_perm('remove_from_group');
-			
-			// get current contentgroup
-			$c_group = new contentgroup();
-			$c_group->generate_from_id($contents[$i-1]->get_contentgroup_id());
-			$thumb_infos['contentgroup_name'] = $c_group->get_name();
-			
-			// check if user has edit perm to that content
-			$thumb_infos['allow_edit'] = $contents[$i-1]->check_perm('edit');
-			// check if user has delete perm to that content
-			$thumb_infos['allow_delete'] = $contents[$i-1]->check_perm('delete');
-			$thumb_infos['place_in_array'] = $i-1;
-			$place_in_cat_array = $contents[$i-1]->get_place_in_cat();
-			$thumb_infos['place_in_cat'] = $place_in_cat_array[$cat_id];
-			if ($contents[$i-1]->get_locked())
-			{
-				$thumb_infos['locked'] = 'checked';
-			}
-		}
-		$array_row[] = $thumb_infos;
-		if ($i % $config_vars['thumb_table_cols'] == 0)
-		{
-			$thumbs[]=$array_row;
-			unset($array_row);
-		}
-		
-	}
-	$thumbs[]=$array_row;
-	$smarty->assign('thumbs',$thumbs);
-	$smarty->assign('is_content', true);
-}*/
+
+
+
 
 $smarty->assign('cat_id',$cat_id);
 
@@ -401,9 +247,7 @@ if (!isset($template_file))
 $smarty->assign('nav_string', build_nav_string($cat_id));
 $smarty->assign('redirect', PHREAKPIC_PATH . "$template_file.php");
 $smarty->assign('thumb_size', $config_vars['thumb_size']['maxsize']);
-$smarty->assign('title_site',$board_config['sitename']);
-$smarty->assign('title_page',$lang['view_cat']);
-$smarty->assign('title_name',$category->get_name());
+
 
 $end_time = getmicrotime();
 $execution_time = $end_time - $start_time;
