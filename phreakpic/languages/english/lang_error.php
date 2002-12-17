ac5e83726d9723de2027105a697a0d91f3132bc9
Tesla2k <Tesla2k>
2002-12-17 18:43:01 +0000

    text for new error handling

---
+++ phreakpic/languages/english/lang_error.php
@@ -0,0 +1,24 @@
+<?php
+// $error['ident'] = 'text';
+
+
+$error['get_place_in_cat'] = 'Couldnt get place_in_cat';
+$error['get_surrounding_content'] = 'Couldnt get surrounding content';
+$error['delete'] = 'Could not delete the object';
+$error['commit'] = 'Could not store the object in the database';
+$error['generate_from_id'] = 'Could not get the objects data';
+$error['fill_content_in_cat'] = 'Could not save categories of content';
+$error['clear_content_in_cat'] = 'Could not clear categorie of content ';
+$error['start_view'] = 'Could not save start of view';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+$error[''] = '';
+
+get_place_in_cat
+?>
