6d3b941cd2e341ca59f45eda8fe7f4e2daaaf874
Tesla2k <Tesla2k>
2002-12-18 00:36:00 +0000

    text for new error handling

--- phreakpic/languages/english/lang_error.php
+++ phreakpic/languages/english/lang_error.php
@@ -6,10 +6,41 @@ $error['get_place_in_cat'] = 'Couldnt get place_in_cat';
 $error['get_surrounding_content'] = 'Couldnt get surrounding content';
 $error['delete'] = 'Could not delete the object';
 $error['commit'] = 'Could not store the object in the database';
-$error['generate_from_id'] = 'Could not get the objects data';
+$error['generate'] = 'Could not get the objects data';
 $error['fill_content_in_cat'] = 'Could not save categories of content';
 $error['clear_content_in_cat'] = 'Could not clear categorie of content ';
 $error['start_view'] = 'Could not save start of view';
+$error['stop_view'] = 'Could not save stop of view';
+$error['cat_commit'] = '';
+$error['del_cat'] = '';
+$error['calc_content_amount'] = '';
+$error['add_user'] = '';
+$error['remove_user'] = '';
+$error['user_in_group'] = '';
+$error['get_childs'] = '';
+$error['generate_initial_for_content'] = '';
+//custom_searches.inc.php
+$error['subsql'] = '';
+$error['action_allowed'] = '';
+$error['get_groups_of_user'] = '';
+$error['get_cats_of_cat'] = '';
+$error['get_content_of_cat'] = '';
+$error['get_content_from_sql'] = '';
+$error['get_cats_data_where_perm'] = '';
+$error['get_catgroups_data_where_perm'] = '';
+$error['get_contentgroups_data_where_perm'] = '';
+$error['get_comments'] = '';
+//statistics.inc.php
+$error['get_views'] = '';
+$error['get_viewed_time'] = '';
+$error['get_content_by_views'] = '';
+$error['get_content_by_viewed_length'] = '';
+$error['get_content_ordered_by'] = '';
+$error['get_custom_searches'] = '';
+$error['custom_search'] = '';
+//admin
+$error['no_admin'] = '';
+$error['get_usergroups_of_user'] = '';
 $error[''] = '';
 $error[''] = '';
 $error[''] = '';
@@ -17,8 +48,8 @@ $error[''] = '';
 $error[''] = '';
 $error[''] = '';
 $error[''] = '';
-$error[''] = '';
-$error[''] = '';
+
+$error['content_not_existing'] = 'The selected Content does not exist';
 
 get_place_in_cat
 ?>
